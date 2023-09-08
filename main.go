711bbc03d447d022f62759e3f08f78fb395f619d
David Taing <adavidtaing@gmail.com>
2023-09-08 21:38:05 +1000

    feat: write request data to stdout

--- main.go
+++ main.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
-	"log"
+	"io"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
+	"os"
+
+	log "github.com/sirupsen/logrus"
 )
+
+type RequestBody struct{}
 
 func main() {
 	var (
@@ -23,19 +29,42 @@ func main() {
 		log.Fatal("Missing required flags: --dest and --port")
 	}
 
+	log.SetFormatter(&log.JSONFormatter{})
+	log.SetOutput(os.Stdout)
+	log.SetLevel(log.InfoLevel)
+
 	target, _ := url.Parse(dest)
 	proxy := httputil.NewSingleHostReverseProxy(target)
 
-	proxy.Director = func(req *http.Request) {
-		req.URL.Scheme = target.Scheme
-		req.URL.Host = target.Host
-		req.Host = target.Host
+	proxy.Director = func(r *http.Request) {
+		r.URL.Scheme = target.Scheme
+		r.URL.Host = target.Host
+		r.Host = target.Host
 	}
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		body, err := io.ReadAll(r.Body)
+		if err != nil {
+			log.Errorf("Error reading request body: %v", err)
+			http.Error(w, "Error reading request body", http.StatusInternalServerError)
+			return
+		}
+
+		r.Body = io.NopCloser(bytes.NewReader(body))
+
+		log.WithFields(log.Fields{
+			"body":   string(body),
+			"method": r.Method,
+			"url":    target.ResolveReference(r.URL).String(),
+		}).Infoln()
+
 		proxy.ServeHTTP(w, r)
 	})
 
-	log.Printf("Starting server on :%d\n", port)
+	log.WithFields(log.Fields{
+		"port": port,
+		"dest": dest,
+	}).Info("Starting server")
+
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), nil))
 }
