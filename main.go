27627d74520b5ac5c293a888f4f3f99232c2b233
David Taing <adavidtaing@gmail.com>
2023-09-08 21:14:11 +1000

    feat: add reverse proxy

--- main.go
+++ main.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	var (
+		dest string
+		port int
+	)
+
+	flag.StringVar(&dest, "dest", "", "")
+	flag.IntVar(&port, "port", 0, "")
+	flag.Parse()
+
+	if dest == "" || port == 0 {
+		log.Fatal("Missing required flags: --dest and --port")
+	}
+
+	target, _ := url.Parse(dest)
+	proxy := httputil.NewSingleHostReverseProxy(target)
+
+	proxy.Director = func(req *http.Request) {
+		req.URL.Scheme = target.Scheme
+		req.URL.Host = target.Host
+		req.Host = target.Host
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		proxy.ServeHTTP(w, r)
+	})
+
+	log.Printf("Starting server on :%d\n", port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), nil))
 }
