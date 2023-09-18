75373d60efa6c01781b995c5a7b6dc3b2e21d726
David Taing <adavidtaing@gmail.com>
2023-09-18 20:15:01 +1000

    feat: add initial version of Fixed Window rate limiting

--- internal/rate_limiter/rate_limiter.go
+++ internal/rate_limiter/rate_limiter.go
@@ -1,12 +1,31 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"log"
 	"net/http"
+	"time"
 )
 
+var bucket = 10
+var refill = 1
+var refillRate time.Duration = 1
+
 func main() {
+	ticker := time.NewTicker(refillRate * time.Second)
+
+	go func() {
+		for range ticker.C {
+			if bucket < 10 {
+				fmt.Println("New token")
+				bucket++
+			}
+
+			fmt.Printf("Bucket: %d, Refresh: %d\n", bucket, refill)
+		}
+	}()
+
 	unlimitedHandler := func(w http.ResponseWriter, r *http.Request) {
 		io.WriteString(w, "There's no limit to the Hello Worlds you'll get!\n")
 	}
@@ -16,6 +35,21 @@ func main() {
 	}
 
 	http.HandleFunc("/unlimited", unlimitedHandler)
-	http.HandleFunc("/limited", limitedHandler)
+	http.Handle("/limited", fixedWindow(http.HandlerFunc(limitedHandler)))
 	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func fixedWindow(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if bucket <= 0 {
+			w.WriteHeader(http.StatusTooManyRequests)
+			io.WriteString(w, http.StatusText(http.StatusTooManyRequests))
+			return
+		}
+
+		fmt.Printf("Bucket: %d, Refresh: %d\n", bucket, refill)
+		bucket--
+		next.ServeHTTP(w, r)
+		log.Println("Request processed")
+	})
 }
